9c9904c452239ef14548180980834eeb3d9b1a90
kinddoctor <tata-yagodina@yandex.ru>
2024-07-28 14:06:54 +0500

    add clean task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,37 @@
+/* eslint-disable arrow-body-style */
+
 import {
   src, dest,
   parallel, series,
-  watch
+  watch,
 } from 'gulp';
-import dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-const sass = gulpSass(dartSass);
 import pug from 'gulp-pug';
 import ttfToWoff2 from 'gulp-ttftowoff2';
 import imagemin from 'gulp-imagemin';
 import browserSync from 'browser-sync';
+import { deleteAsync } from 'del';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+
+const sass = gulpSass(dartSass);
+
+const cleanBuild = () => {
+  return deleteAsync(['build/**', '!build/']);
+};
 
 const compileSCSS = () => {
   return src('src/*.scss')
     .pipe(sass())
     .pipe(dest('build/'))
     .pipe(browserSync.stream());
-}
+};
 
 const compilePug = () => {
   return src('src/*.pug')
     .pipe(pug())
     .pipe(dest('build/'))
     .pipe(browserSync.stream());
-}
+};
 
 const compileFonts = () => {
   return src('src/**/*.ttf')
@@ -39,13 +47,14 @@ const minifyImg = () => {
 
 const browserSyncJob = () => {
   browserSync.init({
-    server: "build/"
+    server: 'build/',
   });
 
   watch('src/*.scss', compileSCSS);
   watch('src/*.pug', compilePug);
-}
+};
 
 export const build = parallel(compileSCSS, compilePug, compileFonts, minifyImg);
 export const server = browserSyncJob;
 export const development = series(build, server);
+export const clean = cleanBuild;
